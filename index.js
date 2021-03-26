30d070fe8901938253936f52e7a26245b7518893
kirstengord <75246798+kirstengord@users.noreply.github.com>
2021-03-26 17:04:37 +0000

    updates

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 document.querySelector("button").addEventListener("click", function() {
 
     var currentImage = document.querySelector("img").getAttribute("src");
-    var newImageNumber = (Math.floor(Math.random()*8) + 1);
+    var newImageNumber = (Math.floor(Math.random()*9) + 1);
     var newImage = "images/quote" + newImageNumber + ".png";
    
     while (currentImage === newImage) {
-        var newImageNumber = (Math.floor(Math.random()*8) + 1);
+        var newImageNumber = (Math.floor(Math.random()*9) + 1);
         var newImage = "images/quote" + newImageNumber + ".png";
     }    
     
